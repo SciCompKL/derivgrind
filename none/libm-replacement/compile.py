170ca8b327ed2a065b7ce1feb4d41676c3552713
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-05-28 18:48:54 +0200

    Changed build procedure of replacement libm

--- none/libm-replacement/compile.py
+++ none/libm-replacement/compile.py
@@ -1,7 +1,8 @@
 import subprocess
 import re
+from overloaded_functions import functions
 
-# Sample program to find libm.so and GLIBC versions.
+# Sample program to find libm.so.
 compile_sample = subprocess.run(["gcc", "sample.c", "-o", "sample", "-m32", "-lm"],universal_newlines=True)
 if compile_sample.returncode!=0:
   raise Exception("Couldn't compile sample C program.")
@@ -19,34 +20,48 @@ for line in ldd_sample.stdout.split("\n"):
 if libmso_name==None or libmso_path==None:
   raise Exception("Couldn't find libm.so with ldd.")
 
-# Parse math.c for all function names.
-glibc_versions = {} # store them as keys here
-with open('math.c','r') as f:
-  for line in f.readlines():
-    r = re.search(r"^DERIVGRIND_MATH_FUNCTION[2ip]*\(([a-zA-Z0-9_]+),", line.strip())
-    if r:
-      glibc_versions[r.group(1)] = None
-
 # Obtain GLIBC versions for all of these functions.
 nm = subprocess.run(["nm","-D","--with-symbol-versions",libmso_path],stdout=subprocess.PIPE,universal_newlines=True)
 for line in nm.stdout.split("\n"):
-  for function in glibc_versions:
-    r = re.search("\s"+function+"@@+(GLIBC_[0-9\._]*\s*$)", line.strip())
+  for function in functions:
+    r = re.search("\s"+function.name+"@@+(GLIBC_[0-9\._]*\s*$)", line.strip())
     if r:
-      glibc_versions[function] = r.group(1)
+      function.glibc_version = r.group(1)
 
 # Produce version script.
 glibc_versions_inv = {}
-for function,glibc_version in glibc_versions.items():
+for function in functions:
+  glibc_version = function.glibc_version
   if glibc_version not in glibc_versions_inv:
     glibc_versions_inv[glibc_version] = []
-  glibc_versions_inv[glibc_version].append(function)
+  glibc_versions_inv[glibc_version].append(function.name)
 version_script = ""
 for glibc_version in glibc_versions_inv:
-  version_script += glibc_version + " { " + "".join([function + "; " for function in glibc_versions_inv]) + " } ; \n"
+  version_script += glibc_version + " { " + "".join([function_name + "; " for function_name in glibc_versions_inv]) + " } ; \n"
 with open("version-script", "w") as f:
   f.write(version_script)
 
+# Produce list of symbols that need to be renamed.
+with open("redefine-syms","w") as f:
+  for function in functions:
+    f.write(function.name+" LIBM_ORIGINAL_"+function.name+"\n")
+
+# Copy libm.so and rename certain symbols.
+adapt_libm = subprocess.run(["objcopy", "--redefine-syms", "redefine-syms", libmso_path, "libmoriginal.so"],universal_newlines=True)
+
+# Produce header file declaring the renamed symbols.
+with open("mathoriginal.h","w") as f:
+  f.write("#define LIBM(name) LIBM_ORIGINAL_##name \n")
+  for function in functions:
+    f.write(function.declaration_of_original())
+
+# Produce source file defining the wrapped math symbols.
+with open("math.c","w") as f:
+  f.write('#include "mathoriginal.h"\n')
+  f.write('#include "valgrind/derivgrind.h"\n')
+  for function in functions:
+    f.write(function.c_code())
+
 # Compile wrapper libm.so.
-compile_libm = subprocess.run(["gcc","math.c",libmso_path.replace(libmso_name,"libm.a"), "-o",libmso_name,"-shared","-fPIC","-Wl,--version-script","version-script","-m32","-I../../install/include"], universal_newlines=True)
+compile_libm = subprocess.run(["gcc","math.c", "-o",libmso_name,"-L.","-lmoriginal","-shared","-fPIC","-Wl,--version-script","version-script","-m32","-I../../install/include"], universal_newlines=True)
 
---
+++ none/libm-replacement/overloaded_functions.py
@@ -0,0 +1,154 @@
+
+# \file overloaded_functions.py
+# Compile to a replacement libm.so, whose functions 
+# - do their original job, and additionally 
+# - update the gradient information.
+# 
+# Access to the gradient information is given through
+# the DerivGrind client requests.
+# 
+# It does not suffice to just wrap machine instructions.
+# For instance, some versions of the glibc libm do not use
+# the x86 fsin instruction to implement sin. Rather,
+# they use several numerical approximations depending on
+# the magnitude of the angle. The derivatives of these 
+# approximations seem to be bad approximations of the
+# derivative sometimes. A replacement libm allows us to
+# use the accurate analytic derivatives.
+#
+# We often need to call math.h functions for the 
+# derivatives, e.g. the derivative of sin is cos. 
+# Therefore we make a copy of the original libm and
+# replace the wrapped function f by LIBM(f).
+#
+# Warning: This file is scanned by the libm-replacement
+# compile script to obtain all names of wrapped functions.
+# Therefore the wrapping must be done by the macros defined 
+# in this file.
+
+class DERIVGRIND_MATH_FUNCTION_BASE:
+  def __init__(self,name,type_):
+    self.name = name
+    self.type = type_
+    self.size = 8 if type_=="double" else 4
+    self.glibc_version = None
+
+class DERIVGRIND_MATH_FUNCTION(DERIVGRIND_MATH_FUNCTION_BASE):
+  """Wrap a math.h function to also handle
+    the derivative information."""
+  def __init__(self,name,deriv,type_):
+    super().__init__(name,type_)
+    self.deriv = deriv
+  def c_code(self):
+    return \
+f"""
+{self.type} {self.name} ({self.type} x){{
+  {self.type} x_d;
+  VALGRIND_GET_DERIVATIVE(&x,&x_d,{self.size});
+  {self.type} ret = LIBM({self.name})(x);
+  {self.type} ret_d = ({self.deriv}) * x_d;
+  VALGRIND_SET_DERIVATIVE(&ret,&ret_d,{self.size});
+  return ret;
+}}
+"""
+  def declaration_of_original(self):
+    return f"""{self.type} LIBM({self.name}) ({self.type});\n"""
+
+class DERIVGRIND_MATH_FUNCTION2(DERIVGRIND_MATH_FUNCTION_BASE):
+  """Wrap a math.h function with two arguments
+    to also handle the derivative information."""
+  def __init__(self,name,derivX,derivY,type_):
+    super().__init__(name,type_)
+    self.derivX = derivX
+    self.derivY = derivY
+  def c_code(self):
+    return \
+f"""
+{self.type} {self.name} ({self.type} x, {self.type} y){{
+  {self.type} x_d, y_d;
+  VALGRIND_GET_DERIVATIVE(&x,&x_d,{self.size});
+  VALGRIND_GET_DERIVATIVE(&y,&y_d,{self.size});
+  {self.type} ret = LIBM({self.name})(x,y);
+  {self.type} ret_d = ({self.derivX}) * x_d + ({self.derivY}) * y_d;
+  VALGRIND_SET_DERIVATIVE(&ret,&ret_d,{self.size});
+  return ret;
+}}
+"""
+  def declaration_of_original(self):
+    return f"""{self.type} LIBM({self.name}) ({self.type}, {self.type});\n"""
+
+class DERIVGRIND_MATH_FUNCTION2e(DERIVGRIND_MATH_FUNCTION_BASE):
+  """Wrap a math.h function on (double,othertype)
+    Used for ldexp and frexp."""
+  def __init__(self,name,deriv,type_,othertype):
+    super().__init__(name,type_)
+    self.deriv = deriv
+    self.othertype = othertype
+  def c_code(self):
+    return \
+f"""
+{self.type} {self.name} ({self.type} x, {self.othertype} e){{
+  {self.type} x_d;
+  VALGRIND_GET_DERIVATIVE(&x,&x_d,{self.size});
+  {self.type} ret = LIBM({self.name})(x,e);
+  {self.type} ret_d = ({self.deriv}) * x_d;
+  VALGRIND_SET_DERIVATIVE(&ret,&ret_d,{self.size});
+  return ret;
+}}
+"""
+  def declaration_of_original(self):
+    return f"""{self.type} LIBM({self.name}) ({self.type}, {self.othertype});\n"""
+
+functions = [
+
+  # missing: modf,fmod
+  DERIVGRIND_MATH_FUNCTION("acos","-1/LIBM(sqrt)(1-x*x)","double"),
+  DERIVGRIND_MATH_FUNCTION("asin","1/LIBM(sqrt)(1-x*x)","double"),
+  DERIVGRIND_MATH_FUNCTION("atan","1/(1+x*x)","double"),
+  DERIVGRIND_MATH_FUNCTION2("atan2","-y/(x*x+y*y)","x/(x*x+y*y)","double"),
+  DERIVGRIND_MATH_FUNCTION("ceil","0","double"),
+  DERIVGRIND_MATH_FUNCTION("cos", "-LIBM(sin)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("cosh", "LIBM(sinh)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("exp", "LIBM(exp)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("fabs", "(x>0?1:-1)","double"),
+  DERIVGRIND_MATH_FUNCTION("floor", "0","double"),
+  DERIVGRIND_MATH_FUNCTION2e("frexp","LIBM(ldexp)(1,-*e)","double","int*"),
+  DERIVGRIND_MATH_FUNCTION2e("ldexp","LIBM(ldexp)(1,e)","double","int"),
+  DERIVGRIND_MATH_FUNCTION("log","1/x","double"),
+  DERIVGRIND_MATH_FUNCTION("log10", "1/(LIBM(log)(10)*x)","double"),
+  DERIVGRIND_MATH_FUNCTION2("pow"," (y==0||y==-0)?0:(y*LIBM(pow)(x,y-1))", "LIBM(pow)(x,y)*LIBM(log)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("sin", "LIBM(cos)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("sinh", "LIBM(cosh)(x)","double"),
+  DERIVGRIND_MATH_FUNCTION("sqrt", "1/(2*LIBM(sqrt)(x))","double"),
+  DERIVGRIND_MATH_FUNCTION("tan", "1/(LIBM(cos)(x)*LIBM(cos)(x))","double"),
+  DERIVGRIND_MATH_FUNCTION("tanh", "1-LIBM(tanh)(x)*LIBM(tanh)(x)","double"),
+
+  # float versions
+  DERIVGRIND_MATH_FUNCTION("acosf","-1/LIBM(sqrtf)(1-x*x)","float"),
+  DERIVGRIND_MATH_FUNCTION("asinf","1/LIBM(sqrtf)(1-x*x)","float"),
+  DERIVGRIND_MATH_FUNCTION("atanf","1/(1+x*x)","float"),
+  DERIVGRIND_MATH_FUNCTION2("atan2f","-y/(x*x+y*y)","x/(x*x+y*y)","float"),
+  DERIVGRIND_MATH_FUNCTION("ceilf","0","float"),
+  DERIVGRIND_MATH_FUNCTION("cosf", "-LIBM(sinf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("coshf", "LIBM(sinhf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("expf", "LIBM(expf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("fabsf", "(x>0?1:-1)","float"),
+  DERIVGRIND_MATH_FUNCTION("floorf", "0","float"),
+  DERIVGRIND_MATH_FUNCTION2e("frexpf","LIBM(ldexpf)(1,-*e)","float","int*"),
+  DERIVGRIND_MATH_FUNCTION2e("ldexpf","LIBM(ldexpf)(1,e)","float","int"),
+  DERIVGRIND_MATH_FUNCTION("logf","1/x","float"),
+  DERIVGRIND_MATH_FUNCTION("log10f", "1/(LIBM(logf)(10)*x)","float"),
+  DERIVGRIND_MATH_FUNCTION2("powf"," (y==0||y==-0)?0:(y*LIBM(powf)(x,y-1))", "LIBM(powf)(x,y)*LIBM(logf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("sinf", "LIBM(cosf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("sinhf", "LIBM(coshf)(x)","float"),
+  DERIVGRIND_MATH_FUNCTION("sqrtf", "1/(2*LIBM(sqrtf)(x))","float"),
+  DERIVGRIND_MATH_FUNCTION("tanf", "1/(LIBM(cosf)(x)*LIBM(cosf)(x))","float"),
+  DERIVGRIND_MATH_FUNCTION("tanhf", "1-LIBM(tanhf)(x)*LIBM(tanhf)(x)","float")
+
+]
+
+
+
+
+
+
