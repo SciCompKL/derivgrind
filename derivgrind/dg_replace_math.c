1a6ab88581417879ec6be8bf4b2e66bff88d79c7
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-07-07 18:01:18 +0200

    Use original function in wrapper in sample vgpreload

--- derivgrind/dg_replace_math.c
+++ derivgrind/dg_replace_math.c
@@ -1,5 +1,11 @@
 #include <valgrind.h>
 double I_WRAP_SONAME_FNNAME_ZZ(Za,testfun)(double x) 
 {
-  return x+1;
+  double res; unsigned long res_i;
+  OrigFn fn;
+  VALGRIND_GET_ORIG_FN(fn);
+  unsigned long x_i = *(unsigned long*)&x;
+  CALL_FN_W_W(res_i, fn, x_i);
+  res = *(double*)&res_i;
+  return res+2;
 }
