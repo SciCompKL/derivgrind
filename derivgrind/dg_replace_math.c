7bf5014224c86c85dc858f62324a302501979631
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-07-08 11:11:04 +0200

    Implement sin, cos wrappers in dg_replace_math.c

--- derivgrind/dg_replace_math.c
+++ derivgrind/dg_replace_math.c
@@ -2,28 +2,43 @@
 #include <derivgrind.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <math.h>
 
 static bool called_from_within_wrapper = false;
 
+__attribute__((optimize("O0")))
 double I_WRAP_SONAME_FNNAME_ZU(Za,sin)(double x) 
 {
   OrigFn fn;
   VALGRIND_GET_ORIG_FN(fn);
-  double res;
-  printf("x=%lf\n",x);
-  volatile double x2=x;
-  CALL_FN_D_D(res, fn, x2);
-  /*if(!called_from_within_wrapper){
-    printf("Enter in sin\n");
-    //VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
+  double ret;
+  CALL_FN_D_D(ret, fn, x);
+  if(!called_from_within_wrapper){
+    double x_d;
+    VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
     called_from_within_wrapper = true;
-      double grad = cos(x) * x_d;
+      double ret_d = cos(x) * x_d;
     called_from_within_wrapper = false;
-    //VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
+    VALGRIND_SET_DERIVATIVE(&ret, &ret_d, 8);
   }
-  */
-  printf("sin x=%lf res=%lf\n", x,res);
-  return res;
+  return ret;
+}
+__attribute__((optimize("O0")))
+double I_WRAP_SONAME_FNNAME_ZU(Za,cos)(double x) 
+{
+  OrigFn fn;
+  VALGRIND_GET_ORIG_FN(fn);
+  double ret;
+  CALL_FN_D_D(ret, fn, x);
+  if(!called_from_within_wrapper){
+    double x_d;
+    VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
+    called_from_within_wrapper = true;
+      double ret_d = -sin(x) * x_d;
+    called_from_within_wrapper = false;
+    VALGRIND_SET_DERIVATIVE(&ret, &ret_d, 8);
+  }
+  return ret;
 }
 /*
 double I_WRAP_SONAME_FNNAME_ZZ(NONE,cosf)(double x) 
