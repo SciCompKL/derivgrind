9c12cddba17e2e45844cc61f6f151d7e4578248c
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-07-08 10:36:07 +0200

    CALL_FN_D_D

--- derivgrind/dg_replace_math.c
+++ derivgrind/dg_replace_math.c
@@ -3,47 +3,49 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-static bool initialization = false;
+static bool called_from_within_wrapper = false;
 
-void I_WRAP_SONAME_FNNAME_ZZ(Za,initmod)(){
-  initialization = !initialization;
-}
-
-static OrigFn fn_sin, fn_cos;
-
-double I_WRAP_SONAME_FNNAME_ZZ(Za,sin)(double x) 
+double I_WRAP_SONAME_FNNAME_ZU(Za,sin)(double x) 
 {
-  unsigned long res_i;
-  printf("Ent sin\n");
-  VALGRIND_GET_ORIG_FN(fn_sin);
-  if(initialization) return 0.;
-  unsigned long x_i = *(unsigned long*)&x;
-  CALL_FN_D_D(res_i, fn_sin, x_i);
-  double res = *(double*)&res_i;
-    double x_d; unsigned long res2_i;
-    CALL_FN_D_D(res2_i, fn_cos, x_i);
-    double res2 = *(double*)&res2_i;
-    VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
-      double grad = res2 * x_d;
-    VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
+  OrigFn fn;
+  VALGRIND_GET_ORIG_FN(fn);
+  double res;
+  printf("x=%lf\n",x);
+  volatile double x2=x;
+  CALL_FN_D_D(res, fn, x2);
+  /*if(!called_from_within_wrapper){
+    printf("Enter in sin\n");
+    //VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
+    called_from_within_wrapper = true;
+      double grad = cos(x) * x_d;
+    called_from_within_wrapper = false;
+    //VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
+  }
+  */
   printf("sin x=%lf res=%lf\n", x,res);
   return res;
 }
-double I_WRAP_SONAME_FNNAME_ZZ(Za,cos)(double x) 
+/*
+double I_WRAP_SONAME_FNNAME_ZZ(NONE,cosf)(double x) 
 {
-  printf("Ent cos\n");
   unsigned long res_i;
-  VALGRIND_GET_ORIG_FN(fn_cos);
-  if(initialization) return 0.;
+  OrigFn fn;
+  VALGRIND_GET_ORIG_FN(fn);
   unsigned long x_i = *(unsigned long*)&x;
-  CALL_FN_D_D(res_i, fn_cos, x_i);
+  CALL_FN_W_W(res_i, fn, x_i);
   double res = *(double*)&res_i;
-    double x_d; unsigned long res2_i;
-    CALL_FN_D_D(res2_i, fn_sin, x_i);
-    double res2 = *(double*)&res2_i;
+  if(!called_from_within_wrapper){
+    printf("Enter in cos\n");
+    double x_d;
     VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
-      double grad = -res2 * x_d;
+    called_from_within_wrapper = true;
+      double grad = -sin(x) * x_d;
+    called_from_within_wrapper = false;
     VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
-  printf("sin x=%lf res=%lf\n", x,res);
+  }
+  printf("cos x=%lf res=%lf\n", x,res);
   return res;
 }
+
+*/
+
