395bc1ccc22f8ddb1686b13970bf83d4b6475d94
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-07-08 09:00:39 +0200

    Save current state

--- derivgrind/dg_replace_math.c
+++ derivgrind/dg_replace_math.c
@@ -1,11 +1,49 @@
 #include <valgrind.h>
-double I_WRAP_SONAME_FNNAME_ZZ(Za,testfun)(double x) 
+#include <derivgrind.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+static bool initialization = false;
+
+void I_WRAP_SONAME_FNNAME_ZZ(Za,initmod)(){
+  initialization = !initialization;
+}
+
+static OrigFn fn_sin, fn_cos;
+
+double I_WRAP_SONAME_FNNAME_ZZ(Za,sin)(double x) 
 {
-  double res; unsigned long res_i;
-  OrigFn fn;
-  VALGRIND_GET_ORIG_FN(fn);
+  unsigned long res_i;
+  printf("Ent sin\n");
+  VALGRIND_GET_ORIG_FN(fn_sin);
+  if(initialization) return 0.;
   unsigned long x_i = *(unsigned long*)&x;
-  CALL_FN_W_W(res_i, fn, x_i);
-  res = *(double*)&res_i;
-  return res+2;
+  CALL_FN_D_D(res_i, fn_sin, x_i);
+  double res = *(double*)&res_i;
+    double x_d; unsigned long res2_i;
+    CALL_FN_D_D(res2_i, fn_cos, x_i);
+    double res2 = *(double*)&res2_i;
+    VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
+      double grad = res2 * x_d;
+    VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
+  printf("sin x=%lf res=%lf\n", x,res);
+  return res;
+}
+double I_WRAP_SONAME_FNNAME_ZZ(Za,cos)(double x) 
+{
+  printf("Ent cos\n");
+  unsigned long res_i;
+  VALGRIND_GET_ORIG_FN(fn_cos);
+  if(initialization) return 0.;
+  unsigned long x_i = *(unsigned long*)&x;
+  CALL_FN_D_D(res_i, fn_cos, x_i);
+  double res = *(double*)&res_i;
+    double x_d; unsigned long res2_i;
+    CALL_FN_D_D(res2_i, fn_sin, x_i);
+    double res2 = *(double*)&res2_i;
+    VALGRIND_GET_DERIVATIVE(&x, &x_d, 8);
+      double grad = -res2 * x_d;
+    VALGRIND_SET_DERIVATIVE(&res, &grad, 8);
+  printf("sin x=%lf res=%lf\n", x,res);
+  return res;
 }
