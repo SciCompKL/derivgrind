b3cf171d554242f417f5b2466c08ad3b33eabc39
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-08-01 15:01:49 +0200

    More copyright and licence notices.

--- derivgrind/diff_tests/python/derivgrind_clientrequests.cpp
+++ derivgrind/diff_tests/python/derivgrind_clientrequests.cpp
@@ -1,3 +1,35 @@
+/*--------------------------------------------------------------------*/
+/*--- Wrap client request            derivgrind_clientrequests.cpp ---*/
+/*--- functions for Python.                                        ---*/
+/*--------------------------------------------------------------------*/
+
+/*
+   This file is part of DerivGrind, a tool performing forward-mode
+   algorithmic differentiation of compiled programs, implemented
+   in the Valgrind framework.
+
+   Copyright (C) 2022 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+   Homepage: https://www.scicomp.uni-kl.de
+   Contact: Prof. Nicolas R. Gauger (derivgrind@scicomp.uni-kl.de)
+
+   Lead developer: Max Aehle (SciComp, TU Kaiserslautern)
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
 #include <pybind11/pybind11.h>
 #include <valgrind/derivgrind.h>
 
