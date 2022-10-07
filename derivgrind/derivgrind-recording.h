15df53afac7cf1c9f57078c19d801f9acaef5eb8
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-10-07 10:56:14 +0200

    Add derivgrind-recording.h with macros for easier registration
    of AD inputs and outputs.

---
+++ derivgrind/derivgrind-recording.h
@@ -0,0 +1,69 @@
+#ifndef DERIVGRIND_RECORDING_H
+#define DERIVGRIND_RECORDING_H
+
+#include <stdio.h>
+
+/*! \file derivgrind-recording.h
+ * Helper macros for simple marking of 
+ * input and output variables in the client code.
+ */
+
+static unsigned long long dg_indextmp, dg_indextmp2;
+static unsigned long long const dg_zero = 0;
+static double const dg_one = 1.;
+
+/*! Mark variable as AD input and assign new 8-byte index to it.
+ * 
+ * You may e.g. write
+ *
+ *     printf("a index = %llu\n", DG_INPUT(a));
+ *
+ * to print the index from your client code. Or use the DG_INPUTF macro to
+ * write it directly into a file.
+ */
+#define DG_INPUT(var) (VALGRIND_NEW_INDEX(&dg_zero,&dg_zero,&dg_zero,&dg_zero,&dg_indextmp), VALGRIND_SET_INDEX(&var,&dg_indextmp), dg_indextmp)
+
+/*! Mark variable as AD input, assign new 8-byte index, and dump the index into a file.
+ */
+#define DG_INPUTF(var) \
+{ \
+  int fd = open("dg-inputs", O_RDWR|O_CREAT|O_APPEND);\
+  unsigned long long index = DG_INPUT(var);\
+  write(fd,&index,8);\
+  close(fd);\
+}
+
+/*! Mark variable as AD output and retrieve its 8-byte index.
+ * 
+ * You may e.g. write
+ *
+ *     printf("a index = %llu\n", DG_OUTPUT(a));
+ *
+ * to print the index from your client code. Or use the DG_OUTPUTF macro to
+ * write it directly into a file.
+ */
+#define DG_OUTPUT(var) (VALGRIND_GET_INDEX(&var,&dg_indextmp2), VALGRIND_NEW_INDEX(&dg_indextmp2,&dg_zero,&dg_one,&dg_zero,&dg_indextmp), dg_indextmp)
+
+/*! Mark variable as AD output, retrieve its 8-byte index, and dump the index into a file.
+ */
+#define DG_OUTPUTF(var) \
+{ \
+  int fd = open("dg-outputs", O_RDWR|O_CREAT|O_APPEND);\
+  unsigned long long index = DG_OUTPUT(var);\
+  write(fd,&index,8);\
+  close(fd);\
+}
+
+/*! Clear the files for the indices of input and output variables.
+ */
+#define DG_CLEARF \
+{ \
+  int fd = open("dg-inputs", O_RDWR|O_CREAT|O_TRUNC);\
+  close(fd); \
+  fd = open("dg-outputs", O_RDWR|O_CREAT|O_TRUNC);\
+  close(fd); \
+}
+
+
+
+#endif
