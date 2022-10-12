901d137960c9eaf6970e4d2163f0ed31134b83fe
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-10-12 18:06:25 +0200

    Remove duplicate code for arg1_part_f etc.
    in gen_operationhandling_code.py.

--- derivgrind/gen_operationhandling_code.py
+++ derivgrind/gen_operationhandling_code.py
@@ -170,15 +170,28 @@ def applyComponentwisely(inputs,outputs,fpsize,simdsize,bodyLowest,bodyNonLowest
     s += f"IRExpr* {outvar} = assembleSIMDVector({outputs[outvar]}_arr, {fpsize}, {simdsize}, diffenv);\n"
   return s
 
-def createBarCode(op, inputs, partials,fpsize,simdsize,llo):
+def createBarCode(op, inputs, floatinputs, partials,fpsize,simdsize,llo):
   """
-    Record an operation on the tape for every component of a SIMD vector.
+    Create C code that records an operation on the tape for every component of a SIMD vector,
+    given the partial derivatives.
+
+    A partial derivative is specified as C code for an IRExpr* of type F64. The C code may involve
+    - the IRExpr* arg1part of type I64 for the respective component of arg1, with the higher 4 bytes
+      being zero if fpsize==4,
+    - the IRExpr* arg1part_f of type F64 for the floating-point value. In the case fpsize==4, the lower
+      4 bytes of arg1part were reinterpreted as F32 and then converted to F64. You need to add the index 1
+      to floatinputs.
+    - The same constructs for arg2, arg3, arg4.
 
     @param op - IROp whose barcode we currently define (necessary to infer the output type).
     @param inputs - List of argument indices (1...4) on which the result depends in a differentiable way.
-    @param partials - List of partial derivatives w.r.t. the arguments in inputs. Each element must be 
-      C code for an IRExpr* of type F64 that evaluates to the partial derivative.
+    @param floatinputs - List of argument indices (1...4) for which the component is needed as F64 arg<i>_part_f.
+    @param partials - List of partial derivatives w.r.t. the arguments in inputs. 
+    @param fpsize - Size of component, either 4 or 8 (bytes)
+    @param simdsize - Number of components, 1, 2, 4 or 8.
+    @param llo - Whether it is a lowest-lane-only operation, boolean.
   """
+  # createBarCode calls applyComponentwisely with the proper input and output vectors and type conversions.
   assert(len(inputs) in [1,2,3])
   input_names = {}
   for i in inputs:
@@ -186,6 +199,15 @@ def createBarCode(op, inputs, partials,fpsize,simdsize,llo):
     input_names[f"i{i}Lo"] = f"i{i}Lo_part"
     input_names[f"i{i}Hi"] = f"i{i}Hi_part"
   output_names = {"indexIntLo":"indexIntLo_part", "indexIntHi":"indexIntHi_part"}
+  # the body computes IRExpr* indexIntLo_part, IRExpr* indexIntHi_part of type I64 with the upper and lower
+  # layer of the index in the respective lower 4 bytes, and zero in the upper four bytes.
+  bodyLowest = ""
+  for i in floatinputs:
+    if fpsize==4:
+      bodyLowest += f'IRExpr* arg{i}_part_f = IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg{i}_part)));\n'
+    else:
+      bodyLowest += f'IRExpr* arg{i}_part_f = IRExpr_Unop(Iop_ReinterpI64asF64,arg{i}_part);'
+  # add statement to push to tape
   if len(inputs)==1: # use index 0 to indicate missing input
     bodyLowest = f'  IRExpr** indexIntHiLo_part = dg_bar_writeToTape(diffenv,i{inputs[0]}Lo_part,i{inputs[0]}Hi_part,IRExpr_Const(IRConst_U64(0)),IRExpr_Const(IRConst_U64(0)), {partials[0]}, IRExpr_Const(IRConst_F64(0.)));\n  IRExpr* indexIntLo_part = indexIntHiLo_part[0];\n  IRExpr* indexIntHi_part = indexIntHiLo_part[1];\n'
   elif len(inputs)==2:
@@ -198,6 +220,7 @@ def createBarCode(op, inputs, partials,fpsize,simdsize,llo):
   else:
     bodyNonLowest = bodyLowest
   barcode = applyComponentwisely(input_names, output_names, fpsize, simdsize, bodyLowest, bodyNonLowest);
+  # cast to the type of the result of the operation
   barcode += f"IRExpr* indexLo = reinterpretType(diffenv,indexIntLo, typeOfIRExpr(diffenv->sb_out->tyenv,{op.apply()}));\n"
   barcode += f"IRExpr* indexHi = reinterpretType(diffenv,indexIntHi, typeOfIRExpr(diffenv->sb_out->tyenv,{op.apply()}));\n"
   return barcode
@@ -229,18 +252,11 @@ for suffix,fpsize,simdsize,llo in [pF64, pF32, p64Fx2, p64Fx4, p32Fx4, p32Fx8, p
   div.dotcode = dv(div.apply(arg1,sub.apply(arg1,mul.apply(arg1,d2,arg3),mul.apply(arg1,arg2,d3)),mul.apply(arg1,arg3,arg3)))
   sqrt.dotcode = dv(div.apply(arg1,d2,mul.apply(arg1,f"mkIRConst_fptwo({fpsize},{simdsize})",sqrt.apply(arg1,arg2))))
 
-  if fpsize==4: # argument values are provided as I64, reinterpret them as F32 or F64 and convert to F64
-  # this F64 value is then used for the computation of the partial derivatives written to the tape
-    arg2_part_f = f"IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,{arg2}_part)))"
-    arg3_part_f = f"IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,{arg3}_part)))"
-  else:
-    arg2_part_f = f"IRExpr_Unop(Iop_ReinterpI64asF64,{arg2}_part)"
-    arg3_part_f = f"IRExpr_Unop(Iop_ReinterpI64asF64,{arg3}_part)"
-  add.barcode = createBarCode(add, [2-llo,3-llo], ["IRExpr_Const(IRConst_F64(1.))", "IRExpr_Const(IRConst_F64(1.))"], fpsize, simdsize, llo)
-  sub.barcode = createBarCode(sub, [2-llo,3-llo], ["IRExpr_Const(IRConst_F64(1.))", "IRExpr_Const(IRConst_F64(-1.))"], fpsize, simdsize, llo)
-  mul.barcode = createBarCode(mul, [2-llo,3-llo], [arg3_part_f, arg2_part_f], fpsize, simdsize, llo)
-  div.barcode = createBarCode(div, [2-llo,3-llo], [f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(1.)),{arg3_part_f})", f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Triop(Iop_MulF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(-1.)), {arg2_part_f}),IRExpr_Triop(Iop_MulF64,dg_bar_rounding_mode,{arg3_part_f},{arg3_part_f}))"], fpsize, simdsize, llo)
-  sqrt.barcode = createBarCode(sqrt, [2-llo], [f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(0.5)), IRExpr_Binop(Iop_SqrtF64,dg_bar_rounding_mode,{arg2_part_f}))"], fpsize, simdsize, llo)
+  add.barcode = createBarCode(add, [2-llo,3-llo], [], ["IRExpr_Const(IRConst_F64(1.))", "IRExpr_Const(IRConst_F64(1.))"], fpsize, simdsize, llo)
+  sub.barcode = createBarCode(sub, [2-llo,3-llo], [], ["IRExpr_Const(IRConst_F64(1.))", "IRExpr_Const(IRConst_F64(-1.))"], fpsize, simdsize, llo)
+  mul.barcode = createBarCode(mul, [2-llo,3-llo], [2-llo, 3-llo], [f"{arg3}_part_f", f"{arg2}_part_f"], fpsize, simdsize, llo)
+  div.barcode = createBarCode(div, [2-llo,3-llo], [2-llo, 3-llo], [f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(1.)),{arg3}_part_f)", f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Triop(Iop_MulF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(-1.)), {arg2}_part_f),IRExpr_Triop(Iop_MulF64,dg_bar_rounding_mode,{arg3}_part_f,{arg3}_part_f))"], fpsize, simdsize, llo)
+  sqrt.barcode = createBarCode(sqrt, [2-llo], [2-llo], [f"IRExpr_Triop(Iop_DivF64,dg_bar_rounding_mode,IRExpr_Const(IRConst_F64(0.5)), IRExpr_Binop(Iop_SqrtF64,dg_bar_rounding_mode,{arg2}_part_f))"], fpsize, simdsize, llo)
 
   IROp_Infos += [add,sub,mul,div,sqrt]
 
@@ -248,57 +264,39 @@ for suffix,fpsize,simdsize,llo in [pF64, pF32, p64Fx2, p64Fx4, p32Fx4, p32Fx8, p
 for suffix,fpsize,simdsize in [("F64",8,1),("F32",4,1),("64Fx2",8,2),("32Fx2",4,2),("32Fx4",4,4)]:
   neg = IROp_Info(f"Iop_Neg{suffix}", 1,[1])
   neg.dotcode = dv(neg.apply("d1"))
-  neg.barcode = createBarCode(neg, [1], ["IRExpr_Const(IRConst_F64(-1.))"], fpsize, simdsize, False)
+  neg.barcode = createBarCode(neg, [1], [], ["IRExpr_Const(IRConst_F64(-1.))"], fpsize, simdsize, False)
   IROp_Infos += [ neg ]
 # Abs 
 for suffix,fpsize,simdsize,llo in [pF64,pF32]: # p64Fx2, p32Fx2, p32Fx4 exist, but AD logic is different
   abs_ = IROp_Info(f"Iop_Abs{suffix}", 1, [1])
   abs_.dotcode = dv(f"IRExpr_ITE(IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{suffix}, arg1, IRExpr_Const(IRConst_{suffix}i(0)))), IRExpr_Unop(Iop_Neg{suffix},d1), d1)")
-  if fpsize==4:  # conversion to F64, see above
-    arg1_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg1_part)))"
-  else:
-    arg1_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg1_part)"
-  abs_.barcode = createBarCode(abs_, [1], [f"IRExpr_ITE( IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{suffix}, {arg1_part_f}, IRExpr_Const(IRConst_{suffix}i(0)))) , IRExpr_Const(IRConst_F64(-1.)), IRExpr_Const(IRConst_F64(1.)))"], fpsize, simdsize, llo)
+  abs_.barcode = createBarCode(abs_, [1], [1], [f"IRExpr_ITE( IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{suffix}, arg1_part_f, IRExpr_Const(IRConst_{suffix}i(0)))) , IRExpr_Const(IRConst_F64(-1.)), IRExpr_Const(IRConst_F64(1.)))"], fpsize, simdsize, llo)
   IROp_Infos += [ abs_ ]
 # Min, Max
 for Op, op in [("Min", "min"), ("Max", "max")]:
   for suffix,fpsize,simdsize,llo in [p32Fx2,p32Fx4,p32F0x4,p64Fx2,p64F0x2,p32Fx8,p64Fx4]:
     the_op = IROp_Info(f"Iop_{Op}{suffix}", 2, [1,2])
     the_op.dotcode = applyComponentwisely({"arg1":"arg1_part","d1":"d1_part","arg2":"arg2_part","d2":"d2_part"}, {"dotvalue":"dotvalue_part"}, fpsize, simdsize, f'IRExpr* dotvalue_part = mkIRExprCCall(Ity_I64,0,"dg_dot_arithmetic_{op}{fpsize*8}", &dg_dot_arithmetic_{op}{fpsize*8}, mkIRExprVec_4(arg1_part, d1_part, arg2_part, d2_part));') 
-    if fpsize==4:  # conversion to F64, see above
-      arg1_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg1_part)))"
-      arg2_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg2_part)))"
-    else:
-      arg1_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg1_part)"
-      arg2_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg2_part)"
-    the_op.barcode = createBarCode(the_op, [1,2], [f"IRExpr_ITE(IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{'F32' if fpsize==4 else 'F64'},{arg1_part_f},{arg2_part_f})),  IRExpr_Const(IRConst_F64({'1.' if op=='min' else '0.'})),  IRExpr_Const(IRConst_F64({'0.' if op=='min' else '1.'})) )",     f"IRExpr_ITE(IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{'F32' if fpsize==4 else 'F64'},{arg1_part_f},{arg2_part_f})),  IRExpr_Const(IRConst_F64({'0.' if op=='min' else '1.'})),  IRExpr_Const(IRConst_F64({'1.' if op=='min' else '0.'})) )"], fpsize, simdsize,llo)
+    the_op.barcode = createBarCode(the_op, [1,2], [1,2], [f"IRExpr_ITE(IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{'F32' if fpsize==4 else 'F64'},arg1_part_f,arg2_part_f)),  IRExpr_Const(IRConst_F64({'1.' if op=='min' else '0.'})),  IRExpr_Const(IRConst_F64({'0.' if op=='min' else '1.'})) )",     f"IRExpr_ITE(IRExpr_Unop(Iop_32to1,IRExpr_Binop(Iop_Cmp{'F32' if fpsize==4 else 'F64'},arg1_part_f,arg2_part_f)),  IRExpr_Const(IRConst_F64({'0.' if op=='min' else '1.'})),  IRExpr_Const(IRConst_F64({'1.' if op=='min' else '0.'})) )"], fpsize, simdsize,llo)
     IROp_Infos += [ the_op ]
 # fused multiply-add
 for Op in ["Add", "Sub"]:
   for suffix,fpsize,simdsize,llo in [pF64,pF32]:
     the_op = IROp_Info(f"Iop_M{Op}{suffix}", 4, [2,3,4])
     the_op.dotcode = dv(f"IRExpr_Triop(Iop_{Op}{suffix}, arg1, IRExpr_Triop(Iop_{Op}{suffix},arg1,d2,arg3), IRExpr_Qop(Iop_M{Op}{suffix},arg1,arg2,d3,d4))")
-    if fpsize==4:  # conversion to F64, see above
-      arg2_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg2_part)))"
-      arg3_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg3_part)))"
-      arg4_part_f = "IRExpr_Unop(Iop_F32toF64,IRExpr_Unop(Iop_ReinterpI32asF32,IRExpr_Unop(Iop_64to32,arg4_part)))"
-    else:
-      arg2_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg2_part)"
-      arg3_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg3_part)"
-      arg4_part_f = "IRExpr_Unop(Iop_ReinterpI64asF64,arg4_part)"
-    the_op.barcode = createBarCode(the_op, [2,3,4], [arg3_part_f, arg2_part_f, f"IRExpr_Const(IRConst_F64({'1.' if Op=='Add' else '-1.'}))"], fpsize, simdsize,llo)
+    the_op.barcode = createBarCode(the_op, [2,3,4], [2,3], ["arg3_part_f", "arg2_part_f", f"IRExpr_Const(IRConst_F64({'1.' if Op=='Add' else '-1.'}))"], fpsize, simdsize,llo)
     IROp_Infos += [ the_op ]
 
 # Miscellaneous
 scalef64 = IROp_Info("Iop_ScaleF64",  3, [2])
 scalef64.dotcode = dv(scalef64.apply("arg1","d2","arg3"))
-scalef64.barcode = createBarCode(scalef64, [2], ["IRExpr_Triop(Iop_ScaleF64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)"], 8, 1, False)
+scalef64.barcode = createBarCode(scalef64, [2], [], ["IRExpr_Triop(Iop_ScaleF64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)"], 8, 1, False)
 yl2xf64 = IROp_Info("Iop_Yl2xF64", 3, [2,3])
 yl2xf64.dotcode = dv("IRExpr_Triop(Iop_AddF64,arg1,IRExpr_Triop(Iop_Yl2xF64,arg1,d2,arg3),IRExpr_Triop(Iop_DivF64,arg1,IRExpr_Triop(Iop_MulF64,arg1,arg2,d3),IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)),arg3)))")
-yl2xf64.barcode = createBarCode(yl2xf64, [2,3], ["IRExpr_Triop(Iop_Yl2xF64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)",  "IRExpr_Triop(Iop_DivF64,arg1,arg2,IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)), arg3))"], 8, 1, False)
+yl2xf64.barcode = createBarCode(yl2xf64, [2,3], [], ["IRExpr_Triop(Iop_Yl2xF64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)",  "IRExpr_Triop(Iop_DivF64,arg1,arg2,IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)), arg3))"], 8, 1, False)
 yl2xp1f64 = IROp_Info("Iop_Yl2xp1F64", 3, [2,3])
 yl2xp1f64.dotcode = dv("IRExpr_Triop(Iop_AddF64,arg1,IRExpr_Triop(Iop_Yl2xp1F64,arg1,d2,arg3),IRExpr_Triop(Iop_DivF64,arg1,IRExpr_Triop(Iop_MulF64,arg1,arg2,d3),IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)),IRExpr_Triop(Iop_AddF64, arg1, arg3, IRExpr_Const(IRConst_F64(1.))))))")
-yl2xp1f64.barcode = createBarCode(yl2xp1f64, [2,3], ["IRExpr_Triop(Iop_Yl2xp1F64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)",  "IRExpr_Triop(Iop_DivF64,arg1,arg2,IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)),IRExpr_Triop(Iop_AddF64, arg1, arg3, IRExpr_Const(IRConst_F64(1.)))))"], 8, 1, False)
+yl2xp1f64.barcode = createBarCode(yl2xp1f64, [2,3], [], ["IRExpr_Triop(Iop_Yl2xp1F64,arg1,IRExpr_Const(IRConst_F64(1.)),arg3)",  "IRExpr_Triop(Iop_DivF64,arg1,arg2,IRExpr_Triop(Iop_MulF64,arg1,IRExpr_Const(IRConst_F64(0.6931471805599453094172321214581)),IRExpr_Triop(Iop_AddF64, arg1, arg3, IRExpr_Const(IRConst_F64(1.)))))"], 8, 1, False)
 IROp_Infos += [ scalef64, yl2xf64, yl2xp1f64 ]
 
 ### Bitwise logical instructions. ###
