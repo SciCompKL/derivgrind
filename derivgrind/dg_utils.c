88b0732e3ff2d2188600f49e4dd9b0d316cbbc46
Max Aehle <max.aehle@scicomp.uni-kl.de>
2022-11-27 18:40:19 +0100

    Working on isZero...

--- derivgrind/dg_utils.c
+++ derivgrind/dg_utils.c
@@ -111,6 +111,19 @@ IRExpr* mkIRConst_fptwo(int fpsize, int simdsize){
   }
 }
 
+IRExpr* isZero(IRExpr* expr, IRType type){
+  switch(type){
+    case Ity_I8: return IRExpr_Binop(Iop_CmpEQ8,expr,IRExpr_Const(IRConst_U8(0)));
+    case Ity_I16: return IRExpr_Binop(Iop_CmpEQ16,expr,IRExpr_Const(IRConst_U16(0)));
+    case Ity_I32: return IRExpr_Binop(Iop_CmpEQ32,expr,IRExpr_Const(IRConst_U32(0)));
+    case Ity_I64: return IRExpr_Binop(Iop_CmpEQ64,expr,IRExpr_Const(IRConst_U64(0)));
+    //case Ity_I128: return IRExpr_Binop(Iop_CmpEQ128,expr,IRExpr_Const(IRConst_U8(0)));
+    case Ity_F32: return IRExpr_Binop(Iop_CmpEQ32,IRExpr_Unop(Iop_ReinterpF32asI32,expr),IRExpr_Const(IRConst_U32(0)));
+    case Ity_F64: return IRExpr_Binop(Iop_CmpEQ64,IRExpr_Unop(Iop_ReinterpF64asI64,expr),IRExpr_Const(IRConst_U64(0)));
+    //case Ity_F128: return IRExpr_Binop(Iop_CmpEQ128,IRExpr_Unop(Iop_ReinterpF128asI128,expr),IRExpr_Const(IRConst_U128(0)));
+  }
+}
+
 IRExpr* getSIMDComponent(IRExpr* expression, int fpsize, int simdsize, int component, DiffEnv* diffenv){
   // reinterpret expression as I32, I64, V128, V256
   IRType type = typeOfIRExpr(diffenv->sb_out->tyenv,expression);
